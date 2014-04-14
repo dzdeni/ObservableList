d8d623a7bda5a8b40bde62071dfe214f403bd8a7
dzdeni <dzdeni@gmail.com>
2014-04-14 17:46:24 +0200

    fix

--- src/hu/co/digital/observablelist/ObservableTest.java
+++ src/hu/co/digital/observablelist/ObservableTest.java
@@ -15,7 +15,6 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ObservableTest {
@@ -27,44 +26,33 @@ public class ObservableTest {
 
 	}
 	
-	@Mock ObservableList<Integer> mockedList;
 	@Mock ChangeListener mockedListener;
 
-	@SuppressWarnings("unchecked")
 	@Before
 	public void setup() {
-		mockedList = mock(ObservableList.class);
 		mockedListener = mock(SimpleChangeListener.class);
 	}
 
 	@Test
-	public void mockAdd() {
-		mockedList.clear();
-		mockedList.add(5);
-		when(mockedList.get(0)).thenReturn(5);
-		assertEquals(5, (int)mockedList.get(0));
-		verify(mockedList, times(1)).add(5);
-	}
-	
-	@Test
-	public void mockClear() {
-		mockedList.clear();
-		mockedList.addAll(Arrays.asList(2, 3, 5, 15));
-		mockedList.clear();
-		assertEquals(null, mockedList.get(0));
+	public void add() {
+		ObservableList<Integer> observableList = new ObservableList<Integer>(new ArrayList<Integer>());
+		observableList.add(2);
+		assertEquals(2, (int) observableList.get(0));
 	}
 
 	@Test
-	public void mockEvent() {
+	public void clear() {
+		ObservableList<Integer> observableList = new ObservableList<Integer>(new ArrayList<Integer>());
+		observableList.addAll(Arrays.asList(2, 3, 5, 15));
+		observableList.clear();
+		assertEquals(0, observableList.size());
+	}
+
+	@Test
+	public void addEvent() {
 		ObservableList<String> observableList = new ObservableList<String>(new ArrayList<String>());
 		observableList.addListener(mockedListener);
-		observableList.add("one");
-		observableList.add(1, "two");
-		observableList.addAll(Arrays.asList("four", "seven"));
-		observableList.addAll(4, Arrays.asList("three", "five"));
-		observableList.remove(0);
-		observableList.removeAll(Arrays.asList("seven", "four"));
-		observableList.clear();
-		verify(mockedListener, times(8)).onObservableListChanged();
+		observableList.add("someThing");
+		verify(mockedListener, times(1)).onObservableListChanged();
 	}
 }
